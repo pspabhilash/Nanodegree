e8f3245ed4c02b853570222805e7a00896a2989b
Abhilash <abhilash.palivela@gmail.com>
2015-12-03 00:31:59 +0530

    Update BookDetail.java
    Fixed orientation change

--- SuperDuo2/alexandria/app/src/main/java/it/jaschke/alexandria/BookDetail.java
+++ SuperDuo2/alexandria/app/src/main/java/it/jaschke/alexandria/BookDetail.java
@@ -111,7 +111,9 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
         shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
         shareIntent.setType("text/plain");
         shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text) + bookTitle);
-         shareActionProvider.setShareIntent(shareIntent);
+         if (shareActionProvider != null) {
+            shareActionProvider.setShareIntent(shareIntent);
+        }
 
         String bookSubTitle = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.SUBTITLE));
         ((TextView) rootView.findViewById(R.id.fullBookSubTitle)).setText(bookSubTitle);
